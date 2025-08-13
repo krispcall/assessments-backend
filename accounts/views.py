780bef20a0979e6104016f9f2d79081c719cf757
Hom Prasad Dhakal <homprasaddhakal>
2025-08-13 10:14:06 +0545

    FEAT: create landing page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def landing_page(request):
+    """ 
+    This view will return static landing page 
+    Args:
+        - request from users
+    Returns:
+        - Render landing page
+    """
+    return render(request, 'accounts/landingpage.html')
--- bookmanagementsystem/urls.py
+++ bookmanagementsystem/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from accounts import views as acc_views
 from django.conf.urls.static import static
 
 
@@ -11,6 +12,7 @@ admin.site.index_title = "Welcome to Book Management System Admin Portal"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', acc_views.landing_page, name="homepage")
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
